ffb7d0cf238c13e623ccf5a8e37e0f7575335e8a
Felipe <fsiqesp2@gmail.com>
2025-01-02 18:39:17 +0000

    parser finalizado mas não roda ainda
    Co-Authored-By: DiegoBangles <110299796+DiegoBangles@users.noreply.github.com>

--- TP2/vibora_lex.py
+++ TP2/vibora_lex.py
@@ -4,30 +4,137 @@ import sys
 literals = [';' , '=', '<', '>','(',')',',','!','+','-','*','/']
 
 tokens = (
-    'NUM',
-    'TEXTO',
-    'VAR'
+    # Tipos da linguagem
+    'NUM', 'TEXTO', 'VAR',
+
+    # Funções
+    'se', 'enquanto', 'ler', 'escreva', 'entao', 'fim', 'senao se', 'senao',
+
+    # Operações Lógicas
+    'e', 'ou', 'nao', 'maior', 'maior_igual', 'menor', 'menor_igual', 'igual', 'diferente', 'E_parentese', 'D_parentese'
+
+    # Operações Aritiméticas
+    'soma', 'subtracao', 'multiplicacao', 'divisao'
 )
 
-def get_lexer():
-    def t_TEXTO(t):
-        r'\"[^"]*\"'
-        return t
+    # Tipos
+def t_TEXTO(t):
+    r'\"[^"]*\"'
+    return t
 
-        
-    def t_NUM(t):
-        r'[0-9]+'
-        #t.value = int(t.value) 
-        return t
-        
-    def t_VAR(t):
-        r'[a-zA-Z][a-zA-Z0-9]*'
-        return t
     
-    t_ignore = ' \n\t'
+def t_NUM(t):
+    r'[0-9]+'
+    #t.value = int(t.value) 
+    return t
+    
+def t_VAR(t):
+    r'[a-zA-Z][a-zA-Z0-9]*'
+    return t
 
-    def t_error(t):
-        print('Illegal character: ', t.value[0])
-        t.lexer.skip(1)
+    # Funções
 
-    return lex.lex()
+def t_se(t):
+    r'se'
+    return t
+
+def t_enquanto(t):
+    r'enquanto'
+    return t
+
+def t_ler(t):
+    r'ler'
+    return t
+
+def t_escreva(t):
+    r'escreva'
+    return t
+
+def t_senao(t):
+    r'senao'
+    return t
+
+def t_entao(t):
+    r'entao'
+    return t
+
+def t_fim(t):
+    r'fim'
+    return t
+
+    #Operações Lógicas
+
+def t_e(t):
+    r'e'
+    return t
+
+def t_ou(t):
+    r'ou'
+    return t
+
+def t_nao(t):
+    r'nao'
+    return t
+
+def t_maior(t):
+    r'>'
+    return t
+
+def t_maior_igual(t):
+    r'>='
+    return t
+
+def t_menor(t):
+    r'<'
+    return t
+
+def t_menor_igual(t):
+    r'<='
+    return t
+
+def t_igual(t):
+    r'=='
+    return t
+
+def t_diferente(t):
+    r'!='
+    return t
+
+def t_E_parentese(t):
+    r'\('
+    return t
+
+def t_D_parentese(t):
+    r'\)'
+    return t
+
+    # Operações Aritiméticas
+
+def t_soma(t):
+    r'\+'
+    return t
+
+def t_subtracao(t):
+    r'\-'
+    return t
+
+def t_multiplicacao(t):
+    r'\*'
+    return t
+
+def t_divisao(t):
+    r'\/'
+    return t
+
+
+t_ignore = ' \n\t'
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_error(t):
+    print('Illegal character: ', t.value[0])
+    t.lexer.skip(1)
+
+lexer = lex.lex()
--- TP2/vibora_parser.py
+++ TP2/vibora_parser.py
@@ -2,148 +2,236 @@ import ply.yacc as yacc
 import sys
 
 from vibora_lex import tokens
-def get_parser():
-    def p_programa(p):
-        f'programa : declaracoes linhas'
 
-    def p_declaracoes(p):
-        f'declaracoes : declaracao'
+def p_programa(p):
+    f'programa : declaracoes linhas'
+    parser.assembly = f'{p[1]} START\n {p[2]}STOP'
 
-    def p_declaracoes2(p):
-        f'declaracoes : declaracoes declaracao'
+def p_programa2(p):
+    f'programa : linhas'
+    parser.assembly = f'START\n {p[1]}STOP'
 
-    def p_declaracao(p):
-        f'declaracao : "inteiro" VAR ;'
+def p_declaracoes(p):
+    f'declaracoes : declaracao'
+    p[0] = f'{p[1]}'
 
-    def p_decaracao2(p):
-        f'declaracao : "inteiro" atribuicao'
+def p_declaracoes2(p):
+    f'declaracoes : declaracoes declaracao'
+    p[0] = f'{p[1]}{p[2]}'
 
-    def p_linhas(p):
-        f'linhas : linha'
-
-    def p_linhas2(p):
-        f'linhas : linhas linha'
-
-    def p_linha(p):
-        f'linha : atribuicao'
-
-    def p_linha2(p):
-        f'linha : comando'
-
-    def p_comando(p):
-        f'comando : cescreva'
-
-    def p_comando2(p):
-        f'comando : cse '
-
-    def p_comando3(p):
-        f'comando : cenquanto'
-
-    def p_comando4(p):
-        f'comando : cler'
-
-    def p_atribuicao(p):
-        f'atribuicao : VAR "=" expressao ";" '
-
-    def p_expressao(p):
-        f'expressao : NUM '
-
-    def p_expressao2(p):
-        f'expressao : VAR '
-    
-    def p_expressao3(p):
-        f'expressao : expressao "+" expressao '
-
-    def p_expressao4(p):
-        f'expressao : expressao "-" expressao'
-
-    def p_expressao5(p):
-        f'expressao : expressao "*" expressao'
-
-    def p_expressao6(p):
-        f'expressao : expressao "/" expressao '
-
-    def p_expressao7(p):
-        f'expressao :  "(" expressao ")'
-
-    def p_cler(p):
-        f'cler : VAR "=" "ler" ( TEXTO ) ";" '
-
-    def p_cescreva(p):
-        f'cescreva : "escreva" ( textvar ) ";" '
-
-    def p_cse(p):
-        f'cse : "se" ( condicoes ) "entao" linhas csezinho '
-
-    def p_csezinho(p):
-        f'csezinho : "senao se" ( condicoes ) "entao" linhas csezinho'
-
-    def p_csezinho2(p):
-        f'csezinho : "senao" linhas "fim" '
-        
-    def p_csezinho3(p):
-        f'csezinho : "fim" '
-
-    def p_cenquanto(p):
-        f'cenquanto : "enquanto" ( condicoes ) "faz" linhas "fim" '
-
-    def p_condicoes(p):
-        f'condicoes : condicao '
-    
-    def p_condicoes2(p):
-        f'condicoes : condicoes "e" condicoes '
-
-    def p_condicoes3(p):
-        f'condicoes : condicoes "ou" condicoes '
-
-    def p_condicoes4(p):
-        f'condicoes : "(" condicoes ")" '
-
-    def p_condicao(p):
-        f'condicao : expressao comparacao expressao '
-
-    def p_comparacao(p):
-        f'comparacao : "=" '
-        
-    def p_comparacao2(p):
-        f'comparacao : "<" '
-
-    def p_comparacao3(p):
-        f'comparacao : ">" '
-
-    def p_comparacao4(p):
-        f'comparacao :  "<="  '
-
-    def p_comparacao5(p):
-        f'comparacao :  ">=" '
-    
-    def p_comparacao6(p):
-        f'comparacao : "!=" '
-
-    def p_textvar(p):
-        f'textvar : textvar "," final '
-
-    def p_textvar2(p):
-        f'textvar : final '
-
-    def p_final(p):
-        f'final : VAR '
-    
-    def p_final2(p):
-        f'final : TEXTO '
-
-    def p_error(p):
-        print("Syntax error in input!")
+def p_declaracao(p):
+    f'declaracao : "inteiro" VAR ;'
+    if p[2] not in p.parser.registers:
+        p.parser.registers.update({p[2]: p.parser.gp})
+        p[0] = f'PUSHI 0\n'
+        p.parser.ints.append(p[2])
+        p.parser.gp += 1
+    else:
+        print("Error: Variavel já existe.")
         parser.exito = False
 
-    parser = yacc.yacc()
-    parser.exito = True
+def p_decaracao2(p):
+    f'declaracao : "inteiro" VAR = NUM ;'
+    if p[2] not in p.parser.registers:
+        p.parser.registers.update({p[2]: p.parser.gp})
+        p[0] = f'PUSHI {p[4]}\n'
+        p.parser.ints.append(p[2])
+        p.parser.gp += 1
+    else:
+        print("Error: Variavel já existe.")
+        parser.exito = False
 
-    fonte = ""
-    for linha in sys.stdin:
-        fonte += linha
 
-    parse = parser.parse(fonte)
+def p_linhas(p):
+    f'linhas : linha'
+    p[0] = f'{p[1]}'
 
-    if parse.exito:
-        print("terminou com sucesso")
+def p_linhas2(p):
+    f'linhas : linhas linha'
+    p[0] = f'{p[1]}{p[2]}'
+
+
+def p_linha(p):
+    f'linha : atribuicao'
+    p[0] = f'{p[1]}'
+
+def p_linha2(p):
+    f'linha : comando'
+    p[0] = f'{p[1]}'
+
+def p_comando(p):
+    f'comando : cescreva'
+    p[0] = f'{p[1]}'
+
+def p_comando2(p):
+    f'comando : cse'
+    p[0] = f'{p[1]}'
+
+def p_comando3(p):
+    f'comando : cenquanto'
+    p[0] = f'{p[1]}'
+
+def p_comando4(p):
+    f'comando : cler'
+    p[0] = f'{p[1]}'
+
+def p_atribuicao(p):
+    f'atribuicao : VAR = expressao ;'
+    p[0] = f'{p[3]} STOREG {p.parser.registers.get(p[1])}\n'
+
+def p_expressao(p):
+    f'expressao : NUM'
+    p[0] = f'PUSHI {p[1]}\n'
+
+def p_expressao2(p):
+    f'expressao : VAR'
+    p[0] = f'PUSHG {p.parser.registers.get(p[1])}\n'
+
+def p_expressao3(p):
+    f'expressao : expressao + expressao '
+    p[0] = f'{p[1]}{p[3]} ADD\n'
+    
+def p_expressao4(p):
+    f'expressao : expressao - expressao'
+    p[0] = f'{p[1]}{p[3]} SUB\n'
+
+def p_expressao5(p):
+    f'expressao : expressao * expressao'
+    p[0] = f'{p[1]}{p[3]} MUL\n'
+
+def p_expressao6(p):
+    f'expressao : expressao / expressao'
+    p[0] = f'{p[1]}{p[3]} DIV\n'
+
+def p_expressao7(p):
+    f'expressao :  ( expressao )'
+    p[0] = f'{p[2]}'
+
+def p_cler(p):
+    f'cler : VAR = "ler" ( TEXTO ) ;'
+    p[0] = f'PUSHS {p[5]}\n WRITES\n READ\n STOREG {p.parser.registers.get(p[1])}\n'
+
+def p_cescreva(p):
+    f'cescreva : "escreva" ( escrevatexto ) ; '
+    p[0] = f'{p[3]}'
+
+def p_cse(p):
+    f'cse : "se" ( condicoes ) "entao" linhas csezinho'
+    p.parser.elses += 1
+    p[0] = f'{p[3]}JZ else{p.parser.elses}\n{p[6]}\nJUMP fim{p.parser.elses} else{p.parser.elses}: \n{p[7]} fim{p.parser.elses}: \n'
+
+def p_csezinho(p):
+    f'csezinho : "senao se" ( condicoes ) "entao" linhas csezinho'
+    p.parser.elses += 1
+    p[0] = f'{p[3]}JZ else{p.parser.elses}\n{p[6]}\nJUMP fim{p.parser.elses} else{p.parser.elses}: \n{p[7]} fim{p.parser.elses}: \n'
+
+def p_csezinho2(p):
+    f'csezinho : "senao" linhas "fim" '
+    p[0] = f'{p[2]}'
+    
+def p_csezinho3(p):
+    f'csezinho : "fim" '
+    p[0] = f''
+
+def p_cenquanto(p):
+    f'cenquanto : "enquanto" ( condicoes ) "faz" linhas "fim" '
+    p.parser.enquantos += 1
+    p[0] = f'inicioloop{p.parser.enquantos}:\n {p[3]}JZ fimloop{p.parser.enquantos}\n{p[6]} JUMP inicioloop{p.parser.enquantos}\n fimloop{p.parser.enquantos}\n'
+
+def p_condicoes(p):
+    f'condicoes : condicao '
+    p[0] = f'{p[1]}'
+
+def p_condicoes2(p):
+    f'condicoes : condicoes "e" condicoes '
+    p[0] = f'{p[1]}{p[3]} AND\n'
+
+def p_condicoes3(p):
+    f'condicoes : condicoes "ou" condicoes '
+    p[0] = f'{p[1]}{p[3]} OR\n'
+
+def p_condicoes4(p):
+    f'condicoes : "(" condicoes ")" '
+    p[0] = f'{p[2]}'
+
+def p_condicao(p):
+    f'condicao : expressao comparacao expressao '
+    p[0] = f'{p[1]}{p[3]}{p[2]}'
+
+def p_comparacao(p):
+    f'comparacao : "==" '
+    p[0] = f'EQUAL\n'
+    
+def p_comparacao2(p):
+    f'comparacao : "<" '
+    p[0] = f'INF\n'
+
+def p_comparacao3(p):
+    f'comparacao : ">" '
+    p[0] = f'SUP\n'
+
+def p_comparacao4(p):
+    f'comparacao :  "<="  '
+    p[0] = f'INFEQ\n'
+
+def p_comparacao5(p):
+    f'comparacao :  ">=" '
+    p[0] = f'SUPEQ\n'
+
+def p_comparacao6(p):
+    f'comparacao : "!=" '
+    p[0] = f'EQUAL\nNOT\n'
+
+def p_escrevatexto(p):
+    f'escrevatexto : escrevatexto "," escrevatextofinal '
+    p[0] = f'{p[1]} PUSHS " " \n WRITES \n{p[3]}'
+
+def p_textvar2(p):
+    f'escrevatexto : escrevatextofinal '
+    p[0] = f'{p[1]} PUSHS "\n"\n WRITES \n'
+
+def p_final(p):
+    f'escrevatextofinal : VAR '
+    p[0] = f'PUSHG {p.parser.registers.get(p[1])}\n WRITEI\n'
+
+def p_final2(p):
+    f'escrevatextofinal : TEXTO '
+    p[0] = f'PUSHS {p[1]}\n WRITES\n'
+
+def p_error(p):
+    print("Syntax error in input!")
+    parser.exito = False
+
+parser = yacc.yacc(debug=True)
+parser.exito = True
+parser.registers = {}
+parser.ints = []
+parser.elses = 0
+parser.enquantos = 0
+parser.gp = 0
+parser.assembly = ""
+
+
+fonte = ""
+for linha in sys.stdin:
+    fonte += linha
+
+parse = parser.parse(fonte)
+
+if parse.exito:
+    print("terminou com sucesso")
+
+try:
+    with open('.\PLC-Projeto\TP2\E1.txt','r') as file:
+        inp = file.read()
+        parser.parse(inp)
+        if parser.success:
+            with open('.\PLC-Projeto\TP2\E1.vm', 'w') as output:
+                output.write(parser.assembly)
+                print(parser.assembly)
+        else:
+            print("<><><><><><><><><><><><><><><><><><><><><><><>")
+            print("Erro")
+            print("<><><><><><><><><><><><><><><><><><><><><><><>")
+except KeyboardInterrupt:
+    print()
